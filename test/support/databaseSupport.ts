f787d654a535668eac504c8fdd76b8ab8823d574
Tyson Gern <tyson@initialcapacity.io>
2024-02-13 20:46:51 -0700

    Extract migrate file function

--- test/support/databaseSupport.ts
+++ test/support/databaseSupport.ts
@@ -3,12 +3,16 @@ import Database from "better-sqlite3";
 import {D1Database, D1DatabaseAPI} from "@miniflare/d1";
 import {D1Database as D1DatabaseType} from "@cloudflare/workers-types"
 
-const migrate = async (db: D1Database): Promise<void> => {
-    const sql = fs.readFileSync('migrations/0000_create_user_tables.sql', {encoding: "utf8"});
+const migrateFile = async (db: D1Database, fileName: string): Promise<void> => {
+    const sql = fs.readFileSync(fileName, {encoding: "utf8"});
     const statements = sql.replaceAll('\n', '').split(';').filter(statement => statement !== '');
     for (const statement of statements) {
         await db.exec(`${statement};`)
     }
+}
+
+const migrate = async (db: D1Database): Promise<void> => {
+    await migrateFile(db, 'migrations/0000_create_user_tables.sql')
 }
 
 export const createDb = async (name: string): Promise<D1DatabaseType> => {
